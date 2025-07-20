ed7d91dd56b9ce615e9937937a7bb7ef97f1b23f
Eric Chiu <echiu12@gmail.com>
2025-07-20 18:14:55 -0400

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 make download
 make tests/test_select && ./tests/test_select > tests/test_select.out
 make tests/bench_plcp && ./tests/bench_plcp > tests/bench_plcp.out
-make tests/bench_bwt_select && ./tests/bench_bwt_select > ./tests/bench_bwt_select
+make tests/bench_bwt_select && ./tests/bench_bwt_select > ./tests/bench_bwt_select.out
 Rscript tests/visualize.R tests
